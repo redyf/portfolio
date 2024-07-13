1b217dd9a633c21dde2e840a948359cc9c54c78c
Redyf <mateusalvespereira7@gmail.com>
2024-07-13 00:02:03 -0300

    refactor: import navbar on layout instead of page component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,13 @@
 import type { Metadata } from "next";
 import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/ui/Navbar";
 
 const jetbrainsMono = JetBrains_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Mateus Alves",
-  description: "Mateus Alves Portfilio",
+  description: "Mateus Alves Portfolio",
   icons: {
     icon: "https://avatars.githubusercontent.com/u/98139059?v=4",
   },
@@ -19,7 +20,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={jetbrainsMono.className}>{children}</body>
+      <body className={jetbrainsMono.className}>
+        <Navbar />
+        {children}
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
 import Personal from "@/components/ui/Personal";
-import Navbar from "@/components/ui/Navbar";
 import Blogs from "@/components/ui/Blogs";
 
 export default function Home() {
   return (
     <>
-      <Navbar />
       <Personal />
       <Blogs />
     </>
