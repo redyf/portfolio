466a0ae07eab9d13c67bdf25736b38b547952561
redyf <mateusalvespereira7@gmail.com>
2024-11-08 17:44:03 -0300

    feat: add getPostData component

---
+++ utils/getPostData.ts
@@ -0,0 +1,28 @@
+import { allPosts, Post } from "contentlayer/generated";
+import { serialize } from "next-mdx-remote/serialize";
+
+export interface PostData {
+  title: string;
+  publishedDate: string;
+  tags: string[];
+  mdxSource: any;
+}
+
+export async function getPostData(slug: string[]): Promise<PostData> {
+  const post = allPosts.find(
+    (post) => post.url === `/posts/${slug.join("/")}`,
+  ) as Post;
+
+  if (!post) {
+    throw new Error("Post not found");
+  }
+
+  const mdxSource = await serialize(post.body.raw); // Use raw for serialization
+
+  return {
+    title: post.title,
+    publishedDate: post.publishedDate,
+    tags: post.tags,
+    mdxSource,
+  };
+}
